8638d85bc917c05b6f8d00e1cb902e2c285631d7
Jaylin16 <jaylin6115@gmail.com>
2023-06-15 23:12:47 +0900

    [JS] 변수 종류

---
+++ week_1/01.js
@@ -0,0 +1,18 @@
+// 변수
+// 1. var
+//재할당 가능.
+//재선언 가능.
+var myVar = "New language JS";
+console.log(myVar);
+
+// 2. let
+//재할당 가능.
+//재선언 불가능.
+let myLet = "New 2";
+console.log(myLet);
+
+// 3. const
+//재할당 불가능.
+//재선언 불가능.
+const myConst = "New 3";
+console.log(myConst);
