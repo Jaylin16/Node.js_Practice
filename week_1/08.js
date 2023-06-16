4026cebb54ef289ca1b13701a28418b69d3db725
Jaylin16 <jaylin6115@gmail.com>
2023-06-17 02:52:03 +0900

    [JS] 화살표 함수

---
+++ week_1/08.js
@@ -0,0 +1,13 @@
+// 화살표 함수
+// ES6 신 문법
+function add(x, y) {
+  return x + y;
+}
+
+// 1-1. 기본적인 화살표 함수
+let arrowFunc01 = (x, y) => {
+  return x + y;
+};
+
+// 1-2. 한줄로 작성하는 법 (내부 로직이 1줄일 때만 가능.)
+let arrowFunc02 = (x, y) => x + y;
